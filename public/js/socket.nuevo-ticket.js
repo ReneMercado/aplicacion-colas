188f88ac93e8f11e62e2d57adacbc7ebc0cd6fab
Rene Mercado <rene.m@financieramas.app>
2018-09-24 22:13:52 -0500

    Primer commit -Produccion

---
+++ public/js/socket.nuevo-ticket.js
@@ -0,0 +1,22 @@
+
+//Comando para establecer la conexión
+var socket = io();
+var label = $('#lblNuevoTicket');
+socket.on('connect', function () {
+
+});
+
+socket.on('disconnect', function () {
+
+});
+
+socket.on('estadoActual', function (resp) {
+    label.text(resp.actual);
+});
+
+
+$('button').on('click', function () {
+    socket.emit('siguienteTicket', null, function (siguienteTicket) {
+        label.text(siguienteTicket);
+    });
+});
